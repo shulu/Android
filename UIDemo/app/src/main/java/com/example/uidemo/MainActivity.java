bb61e804869ea4cc62d7d012522aa2394ec8dbde
shulu <961085397@qq.com>
2023-02-14 16:55:56 +0800

    UIdemo
    UIdemo

--- UIDemo/app/src/main/java/com/example/uidemo/MainActivity.java
+++ UIDemo/app/src/main/java/com/example/uidemo/MainActivity.java
@@ -3,8 +3,12 @@ package com.example.uidemo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
@@ -13,6 +17,9 @@ import android.widget.RatingBar;
 import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.logging.Logger;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
     SeekBar seekBar;
     CheckBox checkBoxYW, checkBoxSX, checkBoxYY;
     RatingBar ratingBar;
+
+    String yw,sx,yy = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,5 +55,105 @@ public class MainActivity extends AppCompatActivity {
         checkBoxSX = findViewById(R.id.cb2);
         checkBoxYY = findViewById(R.id.cb3);
         ratingBar = findViewById(R.id.ratingBar);
+
+        btnLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(R.string.btn1);
+            }
+        });
+
+        btnRight.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                display.setText(R.string.btn2);
+            }
+        });
+
+        aSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    display.setText("On");
+                }else{
+                    display.setText("Off");
+                }
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String s = editText.getText().toString();
+                if (TextUtils.isEmpty(s)){
+                    s = "0";
+                }
+                progressBar.setProgress(Integer.parseInt(s));
+            }
+        });
+
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                Log.d("Test-sl", String.valueOf(checkedId));
+                if (checkedId == R.id.radio_android){
+                    imageView.setImageResource(R.drawable.android);
+                }else{
+                    imageView.setImageResource(R.drawable.apple);
+                }
+            }
+        });
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                display.setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        checkBoxYW.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    yw = "语文";
+                }
+                display.setText(yw+sx+yy);
+            }
+        });
+        checkBoxSX.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    sx = "数学";
+                }
+                display.setText(yw+sx+yy);
+            }
+        });
+        checkBoxYY.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked){
+                    yy = "英语";
+                }
+                display.setText(yw+sx+yy);
+            }
+        });
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                Toast.makeText(getApplicationContext(), String.valueOf(rating)+"星评价!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
