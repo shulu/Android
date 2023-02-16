f2166676f169a9619622b680468c4a87995a754b
shulu <shulu@users.noreply.github.com>
2023-02-16 22:52:39 +0800

    SharedPreferenceDemo
    SharedPreferenceDemo

---
+++ SharedPreferencesDemo/app/src/main/java/com/example/sharedpreferencesdemo/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.sharedpreferencesdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "mytag";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //SharedPreferences shp = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences shp = getSharedPreferences("MY_DATA", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = shp.edit();
+        editor.putInt("MY_DATA", 600);
+        editor.apply();
+
+       //int x = shp.getInt("NUMBER", 0);
+        int x = shp.getInt("MY_DATA", 0);
+
+        Log.d(TAG, "onCreate: "+x);
+    }
+}
